0af764187c5ffb43170d6f6fda5cb4b86878ec6e
mkanoor <mkanoor@redhat.com>
2017-11-13 17:54:44 -0500

    Another Update

--- Testing/Cloud/VM/Provisioning/StateMachines/Methods.class/__methods__/openstack_customizerequest.rb
+++ Testing/Cloud/VM/Provisioning/StateMachines/Methods.class/__methods__/openstack_customizerequest.rb
@@ -4,5 +4,6 @@
 
 # Get provisioning object
 prov = $evm.root["miq_provision"]
+puts "Testing"
 
 $evm.log("info", "Provisioning ID:<#{prov.id}> Provision Request ID:<#{prov.miq_provision_request.id}> Provision Type: <#{prov.provision_type}>")
